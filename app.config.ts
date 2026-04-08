007226c11e994a0649d2d23a0d5a92f9c22a0760
Gian Lorenzo Abano <renz.abano16@gmail.com>
2026-04-09 06:22:23 +0800

    feat: update stale programming image

--- app.config.ts
+++ app.config.ts
@@ -14,52 +14,52 @@ export default defineAppConfig({
         descriptionText: "With over 9 years of programming adventures and 3 years of professional working experience, I bring a treasure chest of technical knowledge and a proven knack for making projects successful!",
         mySkills: [
             {
-                img: "https://www.php.net/images/logos/new-php-logo.svg",
+                img: "/images/skills/php.svg",
                 title: "PHP",
                 description: "Ah, PHP—love it or hate it, this unsung hero powers 60-80% of the web. It's been my trusty sidekick in the industry and a real breadwinner for me!"
             },
             {
-                img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+                img: "/images/skills/javascript.svg",
                 title: "JavaScript",
                 description: "JavaScript is the everywhere, do-everything language of the modern web! From creating magic in browsers to powering backends with Node.js, it’s like the Swiss Army knife of coding."
             },
             {
-                img: "https://icons.iconarchive.com/icons/custom-icon-design/pretty-office-7/256/Sql-runner-icon.png",
+                img: "/images/skills/mysql.svg",
                 title: "Databases",
                 description: "SQL databases are my go-to for organizing data, but I'm no stranger to the NoSQL side of things. My database toolkit includes: PostgreSQL, MySQL, SQLite, CouchDB"
             },
             {
-                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png",
+                img: "/images/skills/html5.svg",
                 title: "HTML5",
                 description: "HTML is the skeleton of the web, and you simply can't build web applications without it. It's simple, powerful, and timeless!"
             },
             {
-                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
+                img: "/images/skills/vuejs.svg",
                 title: "VueJS and NuxtJS",
                 description: "The dynamic duo behind this very portfolio! VueJS and NuxtJS are my favorite tools for building sleek and reactive web applications."
             },
             {
-                img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+                img: "/images/skills/css3.svg",
                 title: "CSS",
                 description: "CSS is the artistry behind every beautiful website. While I’ve dabbled in SCSS and Tailwind, there's nothing quite like the elegance of raw CSS."
             },
             {
-                img: "https://cdn-media-1.freecodecamp.org/images/MiAWLAfns7pPgptcHFFPe8UAwMhPTPp3WWgt",
+                img: "/images/skills/flutter.svg",
                 title: "Flutter & Dart",
                 description: "Creating apps with Flutter & Dart is such a joy! I even tried launching a telehealth startup called HealthMD with this stack. Though funding fell through, the dream lives on!"
             },
             {
-                img: "https://notificare.com/static/5830a2bbcccd513f98e2986d7cc0eee3/c2dd8/cordova-ionic.webp",
+                img: "/images/skills/ionic.svg",
                 title: "Cordova & Ionic",
                 description: "At my current job, I work on an app with Cordova & Ionic, tailored for barcode scanner hardware. It’s tech that makes logistics flow smoothly!"
             },
             {
-                img: "https://bluebirdinternational.com/wp-content/uploads/2023/11/c-sharp-vs-java-bluebird.png",
+                img: "/images/skills/java.svg",
                 title: "Java & C#",
                 description: "Java and C# share such a familiar syntax—it takes me back to my college days, learning ISPROG1. Nostalgia and coding make a great combo!"
             },
             {
-                img: "https://www.svgrepo.com/show/372961/terminal.svg",
+                img: "/images/skills/docker.svg",
                 title: "DevOps & Server Hosting",
                 description: "From wrangling SSH terminals to setting up production servers, I’m your Linux-loving go-to person for all things DevOps and hosting."
             }
